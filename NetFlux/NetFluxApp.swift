4e1b42db670daa77e3a1c6585a16607777679d8f
Adii78560 <raiaditya145@gmail.com>
2026-04-14 01:18:24 +0530

    Initial Commit

---
+++ NetFlux/NetFluxApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetFluxApp.swift
+//  NetFlux
+//
+//  Created by Aditya Rai on 14/04/26.
+//
+
+import SwiftUI
+
+@main
+struct NetFluxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
